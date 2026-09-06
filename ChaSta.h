0590a166d77bd0bdbf3e5b06f2a7d7fbde09ec8b
lordcptriven <lordcptriven@gmail.com>
2026-09-07 01:13:35 +0200

    Create ChaSta.h

---
+++ ChaSta.h
@@ -0,0 +1,25 @@
+#ifndef __CHASTA_H__
+#define __CHASTA_H__
+
+// ĳ���� ���� ����
+#define CH_STAND		1
+#define CH_WALK			2
+#define CH_RUN			4
+#define CH_DIE			8
+#define CH_ATTACK		16
+#define CH_MAGIC		32
+#define CH_SIT			64
+#define CH_SOCIAL		128
+
+// ���ð� ���� 
+#define SM_ST_PEACE			0
+#define SM_WALK_PEACE		1
+#define SM_ATTACK_UNARMED	2
+#define SM_STAND_SIT		3
+#define SM_SIT_STAND		4
+#define SM_SITTING1			5
+#define SM_SITTING2			6
+#define SM_ST_1HAND			7
+#define SM_ST_2SWORD		8
+
+#endif

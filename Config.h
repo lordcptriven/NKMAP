50b4617c0e9368b361506fb1bacb393578c95343
lordcptriven <lordcptriven@gmail.com>
2026-09-07 01:14:15 +0200

    Create Config.h

---
+++ Config.h
@@ -0,0 +1,9 @@
+#ifndef __config_200710241155_theodoric__
+#define __config_200710241155_theodoric__
+
+
+#define TEXTURE_MAX_COUNT 50	// [10/9/2007 Theodoric] �ؽ�ó ������ 15���� 50���� �ø���.
+
+
+
+#endif
